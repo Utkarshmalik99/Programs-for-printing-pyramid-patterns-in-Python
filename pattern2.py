c3b750ac24af6dc65b634e25cec84a7763f43fb4
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-01 03:16:30 +0530

    Create pattern2.py

---
+++ pattern2.py
@@ -0,0 +1,12 @@
+rows=int(input())
+for row in range(1,rows+1):
+    for column in range(1,row +1):
+        print(column,end=" ")
+    print("\r")
+    
+ # 1 
+ # 1 2
+ # 1 2 3 
+ # 1 2 3 4 
+ # 1 2 3 4 5 
+  
