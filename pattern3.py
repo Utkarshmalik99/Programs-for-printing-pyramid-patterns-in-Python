79fef523df37a33db9e8740653a3bf05a1ff84fb
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-02 00:33:58 +0530

    Create pattern3.py

---
+++ pattern3.py
@@ -0,0 +1,8 @@
+rows=int(input())
+#b is used to contain tha numerical values for this pattern
+b=0
+for i in range(rows,0,-1):
+    b=b+1
+    for j in range(1,i+1):
+        print(b,end= " ")
+    print("\r")
