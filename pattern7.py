27d7c5b54f11be015378847169428ddde7f255ca
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-02 01:08:50 +0530

    Create pattern7.py

---
+++ pattern7.py
@@ -0,0 +1,17 @@
+num=int(input("Enter the initial no.: "))
+rows=int(input("Enter the no. of rows: "))
+stop=int(input("Enter the stop value: "))
+
+for row in range(rows):
+    
+    for j in range(1,stop):
+        print(num,end= " ")
+        num+=1
+    print("\r")
+    stop+=1
+
+
+# 1 
+# 2 3 
+# 4 5 6 
+# 7 8 9 10
