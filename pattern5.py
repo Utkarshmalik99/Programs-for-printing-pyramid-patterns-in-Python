07da7bf92d55e5358df46326261437bfc25f1eab
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-02 00:45:20 +0530

    Create pattern5.py

---
+++ pattern5.py
@@ -0,0 +1,7 @@
+rows=int(input())
+# b is used to contain tha numerical values for this pattern
+for i in range(rows,0,-1):
+    b=i
+    for j in range(1,i+1):
+        print(b,end= " ")
+    print("\r")
