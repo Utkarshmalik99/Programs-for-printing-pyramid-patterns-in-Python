ba81916356743f91dc6612dfe5795c7f1b57c6eb
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-02 23:42:08 +0530

    Update pattern3.py

--- pattern3.py
+++ pattern3.py
@@ -2,7 +2,7 @@ rows=int(input())
 #b is used to contain tha numerical values for this pattern
 b=0
 for i in range(rows,0,-1):
-    b=b+1
+    b=b+1 
     for j in range(1,i+1):
         print(b,end= " ")
     print("\r")
