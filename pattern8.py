db7e0965863653d4fc66204dbb1e444d8ac3cd6d
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-03 00:10:29 +0530

    Create pattern8.py

---
+++ pattern8.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range(1, n+1):
+    #for n no. of spaces
+    print(' '*n, end='')
+    # for i no. of *
+    print('* '*(i))
+    # for removing spaces acc to i
+    n-=1
+          #  *   
+         #  *  *   
+         # *  *  *   
+        # *  *  *  *   
