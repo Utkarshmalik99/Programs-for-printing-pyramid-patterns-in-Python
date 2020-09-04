e86d1e21d54f7fc90dfe32c17088768d7f7c59d5
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-04 23:22:23 +0530

    Update pattern4.py

--- pattern4.py
+++ pattern4.py
@@ -1,5 +1,5 @@
 rows=int(input())
-#b is used to contain tha numerical values for this pattern
+#b is used to contain tha numerical values for this pattern 
 b=0 
 for i in range(rows,0,-1):
     b=b+1
