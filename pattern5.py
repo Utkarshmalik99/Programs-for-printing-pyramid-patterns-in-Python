bd5174467da4476734b60c26310f971950706330
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-04 23:21:40 +0530

    Update pattern5.py

--- pattern5.py
+++ pattern5.py
@@ -1,6 +1,6 @@
 rows=int(input())
 # b is used to contain tha numerical values for this pattern
-for i in range(rows,0,-1):
+for i in range(rows,0,-1): 
     b=i
     for j in range(1,i+1):
         print(b,end= " ") 
