2501008449569e8817a0871954c09527f40b499e
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-06 00:09:05 +0530

    Update pattern5.py

--- pattern5.py
+++ pattern5.py
@@ -1,5 +1,5 @@
 rows=int(input())
-# b is used to contain tha numerical values for this pattern
+# b is used to contain tha numerical values for this pattern 
 for i in range(rows,0,-1): 
     b=i
     for j in range(1,i+1):
