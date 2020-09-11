76fe25f15a88abf77c958c37cc778e3247f631d8
Harpreetkaurpanesar25 <52062960+Harpreetkaurpanesar25@users.noreply.github.com>
2020-09-11 21:32:46 +0530

    Update pattern7.py

--- pattern7.py
+++ pattern7.py
@@ -1,10 +1,10 @@
 num=int(input("Enter the initial no.: "))
 rows=int(input("Enter the no. of rows: "))
-stop=int(input("Enter the stop value: "))
+stop=int(input("Enter the stop value: ")) 
 
 for row in range(rows):
     
-    for j in range(1,stop):
+    for j in range(1,stop): 
         print(num,end= " ")
         num+=1 
     print("\r")
