332b0ddb7e55b5bda8c9287143bf7e9667ef1055
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-10-03 16:42:07 +0530

    Update invertedpyramid.py

--- invertedpyramid.py
+++ invertedpyramid.py
@@ -1,8 +1,8 @@
 n=int(input())  
 for i in range(0,n): 
-    # i+1 is initial value to print 1 and n+1 as termination value    
+    # i+1 is the initial value to print 1 and n+1 is the termination value    
     for j in range(i+1,n+1):
         print("*",end=" ")
-    print("\r")
+    print("\r") #for row
 
     
